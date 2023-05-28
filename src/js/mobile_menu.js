c17d571d602ba3c70118be908efc5784f1c90df3
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-28 17:43:25 +0300

    mobile-menu

--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -2,6 +2,11 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeLink1 = document.querySelector('.js-close-link1');
+  const closeLink2 = document.querySelector('.js-close-link2');
+  const closeLink3 = document.querySelector('.js-close-link3');
+  const closeLink4 = document.querySelector('.js-close-link4');
+  const closeLink5 = document.querySelector('.js-close-link5');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +22,16 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeLink1.addEventListener('click', onClick);
+  closeLink2.addEventListener('click', onClick);
+  closeLink3.addEventListener('click', onClick);
+  closeLink4.addEventListener('click', onClick);
+  closeLink5.addEventListener('click', onClick);
+  function onClick(evt) {
+    if (mobileMenu.classList.contains('is-open')) {
+      mobileMenu.classList.remove('is-open');
+    }
+  }
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
