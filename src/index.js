657bdc4c5f4abd5322384efa35992d7adecd9999
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-06-27 23:05:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import './js/mobile_menu';
 
 import './js/scroll_up';
 import './js/feedback';
-// import './js/aos';
-// import './js/aos-set';
 import './js/modal';
-// import './js/loader';
-// import './js/scroll-up';
 import './js/feedback-list';
--- src/js/mobile_menu.js
+++ src/js/mobile_menu.js
@@ -2,11 +2,6 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const closeLink1 = document.querySelector('.js-close-link1');
-  const closeLink2 = document.querySelector('.js-close-link2');
-  const closeLink3 = document.querySelector('.js-close-link3');
-  const closeLink4 = document.querySelector('.js-close-link4');
-  const closeLink5 = document.querySelector('.js-close-link5');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -14,19 +9,19 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    // bodyScrollLock[scrollLockMethod](document.body);
+    //   const scrollLockMethod = !isMenuOpen
+    //     ? 'disableBodyScroll'
+    //     : 'enableBodyScroll';
+    //   // bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  closeLink1.addEventListener('click', onClick);
-  closeLink2.addEventListener('click', onClick);
-  closeLink3.addEventListener('click', onClick);
-  closeLink4.addEventListener('click', onClick);
-  closeLink5.addEventListener('click', onClick);
+  // closeLink1.addEventListener('click', onClick);
+  // closeLink2.addEventListener('click', onClick);
+  // closeLink3.addEventListener('click', onClick);
+  // closeLink4.addEventListener('click', onClick);
+  // closeLink5.addEventListener('click', onClick);
   function onClick(evt) {
     if (mobileMenu.classList.contains('is-open')) {
       mobileMenu.classList.remove('is-open');
