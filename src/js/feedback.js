37eba89e22d3e9dfd92068896408fa050c65286d
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-06-27 13:53:12 +0300

    feedback

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,4 +1,50 @@
 import Swiper from 'swiper';
+
+// Очікуйте, поки сторінка повністю завантажиться
+window.addEventListener('load', function () {
+  // Ініціалізуйте Swiper
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    slidesPerView: 1,
+    // spaceBetween: 10,
+    breakpoints: {
+      //     // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      //     // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      //     // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+    },
+  });
+
+  // Підключення обробників подій для кнопок навігації
+  document
+    .querySelector('.swiper-button-next')
+    .addEventListener('click', function () {
+      swiper.slideNext(); // Перехід до наступного слайда
+    });
+
+  document
+    .querySelector('.swiper-button-prev')
+    .addEventListener('click', function () {
+      swiper.slidePrev(); // Перехід до попереднього слайда
+    });
+});
 // import 'swiper/swiper-bundle.css';
 import { feedback } from './feedback-list';
 
@@ -12,9 +58,8 @@ const markup = feedback
       name,
       stars,
       description,
-    }) => `<div class="feedback-item swiper-slide" data-id=${id}><div class="feedback-container">
-  <img src="${img}" alt="${name}" />
-  <div>
+    }) => `<div class="feedback-item swiper-slide" data-id=${id}><div class="">
+  <div class="feedback-container">
     <h2 class="feedback-title">${name}</h2>
     <img src="${stars}" alt="stars">
   </div>
@@ -26,31 +71,31 @@ const markup = feedback
 
 container_js.insertAdjacentHTML('beforeend', markup);
 
-new Swiper('.feed-cont', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // Default parameters
-  // pagination: false,
-  // slidesPerView: 1,
-  spaceBetween: 10,
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-  },
-});
+// new Swiper('.feed-cont', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   // Default parameters
+//   // pagination: false,
+//   // slidesPerView: 1,
+//   spaceBetween: 10,
+//   // Responsive breakpoints
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20,
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 1,
+//       spaceBetween: 30,
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 40,
+//     },
+//   },
+// });
---
+++ src/js/hkgku.js
@@ -0,0 +1,40 @@
+// const gotopBTN = document.querySelector('.go-top');
+
+// gotopBTN.addEventListener('click', onToTopBtn);
+// function onToTopBtn() {
+//   if (window.pageYOffset > 0) {
+//     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+//     // setTimeout(toTopBtn, 0);
+//   }
+// }
+
+// window.addEventListener('scroll', trackScroll);
+// function trackScroll() {
+//   const offset = window.pageYOffset;
+//   const coords = document.documentElement.clientHeight;
+//   if (offset > coords) {
+//     gotopBTN.classList.add('go-top-show');
+//   } else {
+//     gotopBTN.classList.remove('go-top-show');
+//   }
+// }
+
+// // Get the button:
+// let mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
