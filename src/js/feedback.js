f2a27775fb44baaaa1ce9c7c89fb9d79c90ea8e6
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-06-28 12:38:41 +0300

    slick

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,54 +1,54 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.min.css';
 
 import { feedback } from './feedback-list';
 
-window.addEventListener('load', function () {
-  const swiper = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    grabCursor: true,
-    loop: true,
-    speed: 700,
-    touchEventsTarget: 'wrapper',
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    // slidesPerView: 1,
-    breakpoints: {
-      //     // when window width is >= 320px
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      //     // when window width is >= 480px
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 30,
-      },
-      //     // when window width is >= 640px
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-    },
-  });
+// window.addEventListener('load', function () {
+//   const swiper = new Swiper('.swiper-container', {
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     grabCursor: true,
+//     loop: true,
+//     speed: 700,
+//     touchEventsTarget: 'wrapper',
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     // slidesPerView: 1,
+//     breakpoints: {
+//       //     // when window width is >= 320px
+//       320: {
+//         slidesPerView: 1,
+//         spaceBetween: 30,
+//       },
+//       //     // when window width is >= 480px
+//       480: {
+//         slidesPerView: 1,
+//         spaceBetween: 30,
+//       },
+//       //     // when window width is >= 640px
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 40,
+//       },
+//     },
+//   });
 
-  // Підключення обробників подій для кнопок навігації
-  document
-    .querySelector('.swiper-button-next')
-    .addEventListener('click', function () {
-      swiper.slideNext(); // Перехід до наступного слайда
-    });
+//   // Підключення обробників подій для кнопок навігації
+//   document
+//     .querySelector('.swiper-button-next')
+//     .addEventListener('click', function () {
+//       swiper.slideNext(); // Перехід до наступного слайда
+//     });
 
-  document
-    .querySelector('.swiper-button-prev')
-    .addEventListener('click', function () {
-      swiper.slidePrev(); // Перехід до попереднього слайда
-    });
-});
+//   document
+//     .querySelector('.swiper-button-prev')
+//     .addEventListener('click', function () {
+//       swiper.slidePrev(); // Перехід до попереднього слайда
+//     });
+// });
 
 const container_js = document.querySelector('.js-feedback');
 
@@ -72,3 +72,82 @@ const markup = feedback
   .join('');
 
 container_js.insertAdjacentHTML('beforeend', markup);
+
+// $('.center').slick({
+//   // centerMode: true,
+//   // centerPadding: '450px', // defines other partial showing slides width
+//   slidesToShow: 1,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         // arrows: true,
+//         // centerMode: true,
+//         // centerPadding: '40px',
+//         slidesToShow: 3,
+//       },
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         // arrows: false,
+//         // centerMode: true,
+//         // centerPadding: '40px',
+//         slidesToShow: 1,
+//       },
+//     },
+//   ],
+// });
+$('.carousel').slick({
+  dots: true,
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        // arrows: false,
+        // centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     // arrows: false,
+    //     // centerMode: true,
+    //     // centerPadding: '40px',
+    //     slidesToShow: 1,
+    //   },
+    // },
+  ],
+});
+
+// $(document).ready(function () {
+//   $('.carousel').slick({
+//     centerMode: true,
+//     // centerPadding: '450px', // defines other partial showing slides width
+//     slidesToShow: 1,
+//     dots: true,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3,
+//         },
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1,
+//         },
+//       },
+//     ],
+//   });
+// });
