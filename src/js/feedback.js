2d7623d3deb4ebf067789222fa3fe996a685034b
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-06-30 21:43:14 +0300

    fix

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -13,7 +13,7 @@ const markup = feedback
     }) => `<div class="feedback-item" data-id=${id}><div class="">
   <div class="feedback-container">
     <h2 class="feedback-title">${name}</h2>
-    <img width=66px height=10 src="${stars}" alt="stars">
+    <img width=86px height=10 src="${stars}" alt="stars">
   </div>
 </div>
 <p class="feedback-descr">${description}</p>
