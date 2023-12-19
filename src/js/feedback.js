a5543e110204dda5041a23745819d2a42036fe12
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-12-19 14:00:01 +0200

    hero image

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
       {
         breakpoint: 1199,
         settings: {
-          // arrows: false,
+          arrows: false,
           slidesToShow: 2,
         },
       },
